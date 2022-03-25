31334e8cece509024801ad01f2e4803e3cf6f641
Dang Phong <hpnace2302bn@gmail.com>
2022-03-25 09:57:27 +0700

    update

--- src/Bpo.Dms.WebApp/ClientApp/src/pages/Map.tsx
+++ src/Bpo.Dms.WebApp/ClientApp/src/pages/Map.tsx
@@ -7,10 +7,6 @@ import markerIcon from 'leaflet/dist/images/marker.png';
 
 function MyMap() {
   const [data, setData] = useState([])
-  const bounds = [
-    [51.49, -0.08],
-    [51.5, -0.06],
-  ]
   useEffect(async () => {
     const data = await getStores()
 
@@ -38,7 +34,7 @@ function MyMap() {
               // attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker icon={<FontAwesomeIcon icon="fa-solid fa-location-pin" />} position={[item.location.lat, item.location.lng]}>
+            <Marker icon={markerIconConst} position={[item.location.lat, item.location.lng]}>
               <Popup>
                 {item.name}
               </Popup>
@@ -46,13 +42,13 @@ function MyMap() {
           </div>
         )
       })}
-      <SVGOverlay attributes={{ stroke: 'red' }} bounds={bounds}>
+      {/* <SVGOverlay attributes={{ stroke: 'red' }} bounds={bounds}>
         <rect x="0" y="0" width="100%" height="100%" fill="blue" />
         <circle r="5" cx="10" cy="10" fill="red" />
         <text x="50%" y="50%" stroke="white">
           text
         </text>
-      </SVGOverlay>
+      </SVGOverlay> */}
     </MapContainer>
   )
 }
