98af09d9b5c1c999f02c5f1d12b7c7bac1dfdf42
viethung3597 <37147647+viethung3597@users.noreply.github.com>
2023-07-05 22:11:14 +0700

    Update test.vb

--- test.vb
+++ test.vb
@@ -2,7 +2,12 @@ Imports MySql.Data.MySqlClient
 Public Class Test
     Dim Msg
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-
+            Dim connection As New SqlConnection("Data Source=DESKTOP-JJ2E3DS\SQLEXPRESS;User ID=sa;Password=viethung97;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False")
+            Dim table As New DataTable()
+            Dim a As New SqlCommand("select * from PaymentDetails", connection)
+            Dim adapter As New SqlDataAdapter(a)
+            adapter.Fill(table)
+            DataGridView1.DataSource = table
 
     End Sub
 
